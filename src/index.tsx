d5813f9f167104af3db8971089c2f7470415f1ff
Pranjal Kole <pranjal.kole7@gmail.com>
2024-08-08 04:06:19 +0530

    map

--- src/index.tsx
+++ src/index.tsx
@@ -220,14 +220,7 @@ function Footer({ setState }) {
               <p>+1 800 854-36-80</p>
             </div>
           </div>
-          <iframe
-            width="450"
-            height="250"
-            frameborder="0" style={{border: 0}}
-            referrerpolicy="no-referrer-when-downgrade"
-            src="https://www.google.com/maps/embed/v1/view?key=AIzaSyCTvVfLk-CsgOkH-C6Wwz_rR7FwbnaWfSY&center=-33.8569,151.2152"
-            allowfullscreen>
-          </iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3597.855668290953!2d85.08735887421037!3d25.609711877447698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed5793d036c7e9%3A0xf3c735dd4ca2d604!2sIGIMS!5e0!3m2!1sen!2sin!4v1723070080745!5m2!1sen!2sin" width="400" height="300" style={{ border: 0 }} allowFullScreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           <div class="flex flex-col gap-2">
             <h1 class="text-2xl font-bold">Having a Problem?</h1>
             <input placeholder="Email ID" class="bg-textbox border border-placeholder placeholder:text-placeholder rounded p-2" />
